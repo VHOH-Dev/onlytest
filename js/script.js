c023b42c20e544c6655e1ed62085855a0200645e
Vinicius Henrique <vinyhenrique.dev@gmail.com>
2024-10-03 00:40:20 -0300

    arquivo js incluso

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const consultButton = document.querySelector('.hero button');
+    consultButton.addEventListener('click', function() {
+        alert('Consultoria agendada com sucesso!');
+    });
+});
+
